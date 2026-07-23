a7279c3c9e31123ecac7e680c00dcb8a77802918
JOOALHENDY22 <135716818+JOOALHENDY22@users.noreply.github.com>
2026-07-23 19:23:04 +0300

    Add files via upload

--- Upload_To_GitHub/frontend/src/pages/About.tsx
+++ Upload_To_GitHub/frontend/src/pages/About.tsx
@@ -1,4 +1,4 @@
-import { Pill, Mail, Globe, Github } from 'lucide-react';
+import { Pill, Mail, Globe } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
 
 export default function About() {
